50fca2022fffad3a1f15170843f7d2beb4589232
ensar demirci <demirciensar175@gmail.com>
2026-08-21 11:20:56 +0300

    Extend JWT session timeout to 7 days

--- backend/core/config.py
+++ backend/core/config.py
@@ -20,7 +20,7 @@ class Settings(BaseSettings):
     # Security
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 10080 # 7 gün
 
     @property
     def ASYNC_DATABASE_URI(self) -> str:
